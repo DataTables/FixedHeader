3e93ee92b1519be28a10da15d2aa2d2300b84212
Allan Jardine <allan.jardine@sprymedia.co.uk>
2010-10-30 08:07:05 +0100

    Finish 2.0.4 development

--- js/FixedHeader.js
+++ js/FixedHeader.js
@@ -1,6 +1,6 @@
 /*
  * File:        FixedHeader.js
- * Version:     2.0.4.dev
+ * Version:     2.0.4
  * Description: "Fix" a header at the top of the table, so it scrolls with the table
  * Author:      Allan Jardine (www.sprymedia.co.uk)
  * Created:     Wed 16 Sep 2009 19:46:30 BST
