7e35810fb36673e2bd711f5bbd75ceb4e21d0d9b
Colin Marks <colin@datatables.net>
2019-05-30 11:11:01 +0000

    DEV more unit tests

---
+++ test/option/fixedHeader.headerOffset.js
@@ -0,0 +1,50 @@
+describe('fixedHeader - options - fixedHeader.headerOffset', function() {
+	let table;
+
+	dt.libs({
+		js: ['jquery', 'datatables', 'fixedheader'],
+		css: ['datatables', 'fixedheader']
+	});
+
+	describe('Check the defaults', function() {
+		dt.html('basic');
+		it('Enabled by default...', function() {
+			expect($.fn.dataTable.FixedHeader.defaults.headerOffset).toBe(0);
+		});
+		it('When 0, no fixed header shown on initialisaton', function() {
+			table = $('#example').DataTable({
+				fixedHeader: {
+					headerOffset: 0
+				},
+				paging: false
+			});
+			expect($('table.dataTable').length).toBe(1);
+			expect($('table.fixedHeader-floating').length).toBe(0);
+		});
+		it('... scrolling as expected', async function() {
+			$('html').scrollTop(400);
+			await dt.sleep(500);
+			expect($('table.fixedHeader-floating').offset().top).toBe(400);
+		});
+
+		dt.html('basic');
+		it('When non-zero, no fixed header shown on initialisaton', function() {
+			table = $('#example').DataTable({
+				fixedHeader: {
+					headerOffset: 300
+				},
+				paging: false
+			});
+			expect($('table.dataTable').length).toBe(2);
+			expect($('table.fixedHeader-floating').length).toBe(1);
+		});
+		it('... and at expected offset', function() {
+			expect($('table.fixedHeader-floating').offset().top).toBe(300);
+		});
+		it('... scrolling as expected', async function() {
+			$('html').scrollTop(400);
+			await dt.sleep(500);
+			expect($('table.fixedHeader-floating').offset().top).toBe(700);
+		});
+	});
+});
