46af83cb547af9ba49988c26365679cae53634fd
Allan Jardine <allan@datatables.net>
2021-10-12 07:50:33 +0000

    Fix: Set the height of the target table's scrolling header container only
    Fixes #106

--- js/dataTables.fixedHeader.js
+++ js/dataTables.fixedHeader.js
@@ -280,7 +280,7 @@ $.extend( FixedHeader.prototype, {
 		} );
 
 		this._positions();
-		$('div.dataTables_scrollHeadInner').height(this.s.position.theadHeight);
+		$('div.dataTables_scrollHeadInner', dt.table().container()).height(this.s.position.theadHeight);
 		this._scroll();
 	},
 
