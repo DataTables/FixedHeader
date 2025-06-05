5f95cdce65325f15da3b89ed38f9099fa67c6d91
Allan Jardine <allan@datatables.net>
2025-06-05 11:25:30 +0000

    Dev: Include the item in the new fixedheader-mode event

--- js/dataTables.fixedHeader.js
+++ js/dataTables.fixedHeader.js
@@ -671,7 +671,7 @@ $.extend(FixedHeader.prototype, {
 		this.s.scrollLeft.footer = -1;
 		this.s[item + 'Mode'] = mode;
 
-		dt.trigger('fixedheader-mode', [mode]);
+		dt.trigger('fixedheader-mode', [mode, item]);
 	},
 
 	/**
