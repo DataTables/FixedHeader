fa0c535d59480d33ef9995c9fb3c2554dd9d3589
Allan Jardine <allan@datatables.net>
2025-10-28 13:09:18 +0000

    Dev: Fast scrolling could cause an error

--- js/dataTables.fixedHeader.js
+++ js/dataTables.fixedHeader.js
@@ -627,7 +627,10 @@ $.extend(FixedHeader.prototype, {
 				});
 
 			importantWidth(position.width);
-			scrollLeftUpdate();
+
+			if (scrollLeftUpdate) {
+				scrollLeftUpdate();
+			}
 
 			if (item === 'footer') {
 				itemDom.floating.css('top', '');
