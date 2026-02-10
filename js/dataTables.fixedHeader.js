3defb25dbfe1533dded755f614e2c5ecfcd0226c
Allan Jardine <allan@datatables.net>
2026-02-10 17:10:46 +0000

    Fix: "Jitter" when scrolling in Chrome due to table DOM structure change
    gh#105

--- js/dataTables.fixedHeader.js
+++ js/dataTables.fixedHeader.js
@@ -436,9 +436,17 @@ $.extend(FixedHeader.prototype, {
 			itemDom.placeholder = itemElement.clone(false);
 			itemDom.placeholder.find('*[id]').removeAttr('id');
 
-			// Move the thead / tfoot elements around - original into the floating
-			// element and clone into the original table
-			itemDom.host.prepend(itemDom.placeholder);
+			// Move the thead / tfoot elements around - original into the
+			// floating element and clone into the original table. Note that the
+			// order is important in Chrome. It must be colgroup, thead, tbody,
+			// tfoot. Otherwise a "jitter" when scrolling will occur.
+			$(itemDom.placeholder).insertAfter(
+				item === 'header'
+					? $('colgroup', itemDom.host)
+					: $('tbody', itemDom.host)
+			);
+
+			// itemDom.host.prepend(itemDom.placeholder);
 			itemDom.floating.append(itemElement);
 
 			this._widths(itemDom);
@@ -561,7 +569,7 @@ $.extend(FixedHeader.prototype, {
 
 			if (!$.contains(itemDom.host[0], tablePart[0])) {
 				if (item === 'header') {
-					itemDom.host.prepend(tablePart);
+					tablePart.insertAfter($('colgroup', itemDom.host));
 				}
 				else {
 					itemDom.host.append(tablePart);
