aac6e2480ee3a88a76b71a1f8be940ae9f53526b
Allan Jardine <allan@datatables.net>
2026-09-22 10:34:40 +0000

    Release 5.1.0

--- js/FixedHeader.ts
+++ js/FixedHeader.ts
@@ -19,7 +19,7 @@ export default class FixedHeader {
 	};
 
 	/** Version */
-	static version = '5.0.0';
+	static version = '5.1.0';
 
 	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 	 * Public methods (exposed via the DataTables API below)
